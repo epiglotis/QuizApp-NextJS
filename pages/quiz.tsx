3afff8f960d7cec26d000b4d35517a445e9c2ae4
epiglotis <83986620+epiglotis@users.noreply.github.com>
2023-08-09 16:27:45 +0300

    Questions Added

--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -1,6 +1,60 @@
 import React from 'react'
 
 const QuizPage:React.FC = () => {
+
+    const questions = [
+  {
+    question: 'What is the capital of France?',
+    options: ['Paris', 'Berlin', 'Madrid', 'Rome'],
+    answer: 'Paris',
+  },
+  {
+    question: 'Which planet is known as the Red Planet?',
+    options: ['Mars', 'Jupiter', 'Venus', 'Mercury'],
+    answer: 'Mars',
+  },
+  {
+    question: 'What is the largest mammal?',
+    options: ['Elephant', 'Blue Whale', 'Giraffe', 'Lion'],
+    answer: 'Blue Whale',
+  },
+  {
+    question: 'Which gas do plants use for photosynthesis?',
+    options: ['Oxygen', 'Carbon Dioxide', 'Nitrogen', 'Hydrogen'],
+    answer: 'Carbon Dioxide',
+  },
+  {
+    question: 'Who painted the Mona Lisa?',
+    options: ['Pablo Picasso', 'Leonardo da Vinci', 'Vincent van Gogh', 'Michelangelo'],
+    answer: 'Leonardo da Vinci',
+  },
+  {
+    question: 'Which instrument is known as the "king of instruments"?',
+    options: ['Piano', 'Violin', 'Trumpet', 'Organ'],
+    answer: 'Organ',
+  },
+  {
+    question: 'What is the chemical symbol for gold?',
+    options: ['Ag', 'Au', 'Gd', 'Fe'],
+    answer: 'Au',
+  },
+  {
+    question: 'What is the largest planet in our solar system?',
+    options: ['Earth', 'Mars', 'Jupiter', 'Venus'],
+    answer: 'Jupiter',
+  },
+  {
+    question: 'Which country is known as the "Land of the Rising Sun"?',
+    options: ['China', 'South Korea', 'Japan', 'Vietnam'],
+    answer: 'Japan',
+  },
+  {
+    question: 'Which famous scientist developed the theory of relativity?',
+    options: ['Isaac Newton', 'Albert Einstein', 'Galileo Galilei', 'Stephen Hawking'],
+    answer: 'Albert Einstein',
+  },
+];
+
   return (
     <div>
       <h1>Interactive Quiz</h1>
