5cb0fb2c5c77b20dd4a36518224685e7c284e11d
epiglotis <83986620+epiglotis@users.noreply.github.com>
2023-08-09 16:27:23 +0300

    QuizPage Created

---
+++ pages/quiz.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const QuizPage:React.FC = () => {
+  return (
+    <div>
+      <h1>Interactive Quiz</h1>
+      <p>This is your interactive quiz page.</p>
+    </div>
+  )
+}
+
+export default QuizPage
